6bba8aa8f223d92683d83acc448021d9b0758efe
dabrown104 <raven122685@yahoo.com>
2016-04-25 22:50:35 -0400

    Assignment1 Seminar4
    This is an example of text wrapping and different text
    changes/background changes.

---
+++ Greetings/Greetings/Greetings/GreetingsPage.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Greetings
+{
+    public partial class GreetingsPage : ContentPage
+    {
+        public GreetingsPage()
+        {
+            Content = new Label
+            {
+                FormattedText = new FormattedString
+                {
+                    Spans =
+                    {
+                    new Span
+                    {
+                    Text = "Please take a look at this "
+                    },
+                    new Span
+                    {
+                    Text = "very",
+                    FontAttributes = FontAttributes.Bold
+                    },
+                    new Span
+                    {
+                    Text = " important story about changing " +
+                    "so much within a text. "
+                    },
+                    new Span
+                    {
+                    Text =
+                    " This is a combination of " +
+                    "span features and wrapping text " +
+                    "within the same program." +
+                    "You will also see how to change " +
+                    "the text color and the background "
+                    },
+                    }
+                },
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                BackgroundColor = Color.Green,
+                TextColor = Color.Blue
+            };
+
+            Padding = new Thickness(5, Device.OnPlatform(20, 5, 5), 5, 5);
+
+        }
+    }
+}
